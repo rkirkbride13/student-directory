99006bae9ef84708bb7168275788d0930ff02ab8
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2022-11-01 11:56:05 +0000

    Saving to csv process added

--- directory.rb
+++ directory.rb
@@ -32,6 +32,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to stundents.csv"
   puts "9. Exit"
 end
 
@@ -47,6 +48,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -60,5 +63,18 @@ def interactive_menu
     process(gets.chomp)
   end
 end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 
 interactive_menu
