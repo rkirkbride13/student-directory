fc7ef5b0313658db21a5b3331fa2b1dc08687d4c
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2022-11-01 10:26:33 +0000

    Refactor of methods and instance variable added

--- directory.rb
+++ directory.rb
@@ -1,55 +1,63 @@
+@students = [] # an empty array accessible to all methods
+
 def print_header
     puts "The students of Villains Academy"
     puts "-------------"
 end
 
-def print(students)
-    students.each do |student|
+def print_students_list
+    @students.each do |student|
         puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
-def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
+def print_footer
+    puts "Overall, we have #{@students.count} great students"
 end
 
 def input_students
     puts "Please enter the name of the students"
     puts "To finish, just hit return twice"
-    # create an empty array
-    students = []
     # get the first name
     name = gets.chomp
     # while the name is not empty, repeat this code
     while !name.empty? do
         # add the student hash to the array
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
+        @students << {name: name, cohort: :november}
+        puts "Now we have #{@students.count} students"
         name = gets.chomp
     end
-    # return the array of students
-    students
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
 end
 
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
